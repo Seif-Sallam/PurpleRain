cdffdc1fbb240e8f7a3a5b16b044d601f21570c0
Seif-Sallam <seif_sallam@aucegypt.edu>
2024-02-06 23:53:36 +0200

    purple rain :)

--- PurpleRain/src/PurpleRain/main.cpp
+++ PurpleRain/src/PurpleRain/main.cpp
@@ -1,4 +1,21 @@
 #include "raylib.h"
+#include <string>
+
+constexpr const Vector2 RAIN_DROP_SIZE = Vector2{5, 15};
+constexpr const Color RAIN_DROP_COLOR = Color{128, 0, 128, 255};
+constexpr const float GRAVITY = 0.01f;
+
+struct RainDrop {
+	// The Z component will be used even though we are in 2D
+	Vector3 position;
+	float speed;
+};
+
+void InitRainDrop(RainDrop& rainDrop);
+void UpdateRainDrop(RainDrop& rainDrop, float deltaTime);
+void DrawRainDrop(RainDrop& rainDrop);
+
+void DrawFPS(float dt);
 
 int main(void)
 {
@@ -7,18 +24,36 @@ int main(void)
 
 	InitWindow(screenWidth, screenHeight, "Purple Rain");
 
+	RainDrop drops[1000] = {};
+	for (auto& drop : drops)
+		InitRainDrop(drop);
+
 	SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
 
 	// Remove exit by Esc
 	SetExitKey(KEY_NULL);
 
+	float deltaTime = 1.0f;
+	bool drawText = false;
+
 	// Main game loop
 	while (!WindowShouldClose())
 	{
+		deltaTime = GetFrameTime();
+
+		if (IsKeyPressed(KEY_D))
+			drawText = !drawText;
+
+		for (auto& drop : drops)
+			UpdateRainDrop(drop, deltaTime);
 
 		BeginDrawing();
 		{
-			ClearBackground(Color{168, 168, 168, 255});
+			ClearBackground(BLACK);
+			for (auto& drop : drops)
+				DrawRainDrop(drop);
+			if (drawText)
+				DrawFPS(deltaTime);
 		}
 		EndDrawing();
 	}
@@ -26,4 +61,52 @@ int main(void)
 	CloseWindow();
 
 	return 0;
+}
+
+float map(float value, float minV, float maxV, float otherMin, float otherMax)
+{
+	// Maps the value between other value range
+	return ((value / (minV + maxV)) * otherMax) + otherMin;
+}
+
+void InitRainDrop(RainDrop& rainDrop)
+{
+	rainDrop.position.x = (float)GetRandomValue(3, GetRenderWidth() - 3);
+	rainDrop.position.y = (float)GetRandomValue(-50, 10);
+
+	static const int MAX_SPEED = 5;
+	static const int MIN_SPEED = 0;
+	static const int Z_MIN = 1;
+	static const int Z_MAX = 11;
+
+	rainDrop.position.z = (float)GetRandomValue(Z_MIN, Z_MAX);
+
+	rainDrop.speed = map(rainDrop.position.z, Z_MIN, Z_MAX, MIN_SPEED, MAX_SPEED);
+}
+
+void UpdateRainDrop(RainDrop& rainDrop, float deltaTime)
+{
+	rainDrop.speed += rainDrop.speed * GRAVITY;
+	rainDrop.position.y += rainDrop.speed;
+
+	// If we are out of bounds
+	if (rainDrop.position.y >= GetRenderHeight())
+	{
+		// give it a random position
+		InitRainDrop(rainDrop);
+	}
+}
+
+void DrawRainDrop(RainDrop& drop)
+{
+	DrawRectangle((int)drop.position.x, (int)drop.position.y, (int)RAIN_DROP_SIZE.x, (int)RAIN_DROP_SIZE.y, RAIN_DROP_COLOR);
+}
+
+void DrawFPS(float deltaTime)
+{
+	std::string latency = "Latency: " + std::to_string(deltaTime);
+	std::string fps = "FPS: " + std::to_string(GetFPS());
+
+	DrawText(latency.c_str(), 10, 0, 30, RAYWHITE);
+	DrawText(fps.c_str(), 10, 40, 30, RAYWHITE);
 }
