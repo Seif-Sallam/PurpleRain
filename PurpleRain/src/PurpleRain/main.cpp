cff49bd0d45998d06a60b67c442ffbe48ac1fa01
Seif-Sallam <seif_sallam@aucegypt.edu>
2024-02-07 00:15:41 +0200

    WIND!

--- PurpleRain/src/PurpleRain/main.cpp
+++ PurpleRain/src/PurpleRain/main.cpp
@@ -8,12 +8,13 @@ constexpr const float GRAVITY = 0.01f;
 struct RainDrop {
 	// The Z component will be used even though we are in 2D
 	Vector3 position;
-	float speed;
+	float speed; // in the Y direction
 };
 
 void InitRainDrop(RainDrop& rainDrop);
-void UpdateRainDrop(RainDrop& rainDrop, float deltaTime);
+void UpdateRainDrop(RainDrop& rainDrop, float windSpeed, float deltaTime);
 void DrawRainDrop(RainDrop& rainDrop);
+float GetWindSpeed();
 
 void DrawFPS(float dt);
 
@@ -36,16 +37,22 @@ int main(void)
 	float deltaTime = 1.0f;
 	bool drawText = false;
 
+	float windSpeed = 0.0f; // X Speed
+
 	// Main game loop
 	while (!WindowShouldClose())
 	{
 		deltaTime = GetFrameTime();
 
-		if (IsKeyPressed(KEY_D))
+		if (IsKeyPressed(KEY_SPACE))
 			drawText = !drawText;
+		if (IsKeyPressed(KEY_D) || IsKeyDown(KEY_D))
+			windSpeed += 0.01f;
+		if (IsKeyPressed(KEY_A) || IsKeyDown(KEY_A))
+			windSpeed -= 0.01f;
 
 		for (auto& drop : drops)
-			UpdateRainDrop(drop, deltaTime);
+			UpdateRainDrop(drop, windSpeed, deltaTime);
 
 		BeginDrawing();
 		{
@@ -84,9 +91,10 @@ void InitRainDrop(RainDrop& rainDrop)
 	rainDrop.speed = map(rainDrop.position.z, Z_MIN, Z_MAX, MIN_SPEED, MAX_SPEED);
 }
 
-void UpdateRainDrop(RainDrop& rainDrop, float deltaTime)
+void UpdateRainDrop(RainDrop& rainDrop, float windSpeed, float deltaTime)
 {
 	rainDrop.speed += rainDrop.speed * GRAVITY;
+	rainDrop.position.x += windSpeed;
 	rainDrop.position.y += rainDrop.speed;
 
 	// If we are out of bounds
@@ -100,6 +108,20 @@ void UpdateRainDrop(RainDrop& rainDrop, float deltaTime)
 void DrawRainDrop(RainDrop& drop)
 {
 	DrawRectangle((int)drop.position.x, (int)drop.position.y, (int)RAIN_DROP_SIZE.x, (int)RAIN_DROP_SIZE.y, RAIN_DROP_COLOR);
+}
+
+float GetWindSpeed()
+{
+	auto mousePosition = GetMousePosition();
+	if(mousePosition.x < 0)
+		mousePosition.x = 0;
+	else if (mousePosition.x > GetRenderWidth())
+		mousePosition.x = (float)GetRenderWidth();
+	float middleOfScreen = GetRenderWidth() / 2.0f;
+
+	auto delta = int(mousePosition.x - middleOfScreen) % 100; // Max speed in the X
+
+	return (float) 5.0f * delta / 100;
 }
 
 void DrawFPS(float deltaTime)
